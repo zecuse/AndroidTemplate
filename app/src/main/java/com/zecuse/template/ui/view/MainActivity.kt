8184554350c28a72f26caa90bb2acc3d1fe3f020
Cory Williamson <zekos@ymail.com>
2024-07-04 16:39:28 -0400

    Added loading of settings from local database with room.

--- app/src/main/java/com/zecuse/template/ui/view/MainActivity.kt
+++ app/src/main/java/com/zecuse/template/ui/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,10 +12,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import com.zecuse.template.database.SettingsDatabase
 import com.zecuse.template.ui.theme.TimepiecesTheme
+import com.zecuse.template.viewmodel.SettingsFactory
+import com.zecuse.template.viewmodel.SettingsViewModel
 
 class MainActivity: ComponentActivity()
 {
+	private val db by lazy {
+		Room.databaseBuilder(context = applicationContext,
+		                     klass = SettingsDatabase::class.java,
+		                     name = "settings.db")
+			.build()
+	}
+
+	private val settingsModel by viewModels<SettingsViewModel>(factoryProducer = {SettingsFactory(db)})
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
